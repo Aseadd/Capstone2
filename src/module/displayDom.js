0e1cdae3baa39cfddde9787faefd4581595dc839
addis <betsega23@gmail.com>
2022-08-31 22:42:48 +0300

    Change image sizes and card

--- src/module/displayDom.js
+++ src/module/displayDom.js
@@ -10,8 +10,8 @@ const displayDom = async (url) => {
   for (let i = 0; i < meals.meals.length; i += 1) {
     mealsHtml += ` <div class = 'col mt-4 pe-0'>
                     <div class="card" style="width: 20rem;">
-                    <img src="${meals.meals[i].strMealThumb}" class="card-img-top" alt="...">
-                    <div class="card-body text-white ">
+                    <img src="${meals.meals[i].strMealThumb}" class="card-img-top img" alt="...">
+                    <div class="card-body text-black ">
                      <div class = "title-like"> 
                      <h5 class="card-title"> ${meals.meals[i].strMeal}</h5>
                      <span id="like${meals.meals[i].idMeal}" class="ms-4 like">🖤</span>
