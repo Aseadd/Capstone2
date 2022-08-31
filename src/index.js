f6224cbc1b646759459c825a9edaeb3adec148cf
addis <betsega23@gmail.com>
2022-08-31 21:02:41 +0300

    Add interface and lists

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 // import _ from 'lodash';
 import './style.css';
+import displayDom from './module/displayDom.js';
+import { isMatch } from 'lodash';
+
+const image =
+  'https://www.themealdb.com/images/media/meals/xvsurr1511719182.jpg';
+
+const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=a';
+displayDom(url);
+
+// const card = document.querySelector('.meal-img');
+// const img = document.createElement('img');
+// img.classList.add('card-img-top');
+// img.setAttribute('src', image);
+// card.appendChild(img);
---
+++ src/module/displayDom.js
@@ -0,0 +1,29 @@
+import getMeals from './mealApi.js';
+
+const displayDom = async (url) => {
+  const result = document.querySelector('.row');
+  const meals = await getMeals(url);
+  // const mealList = JSON.parse(meals);
+  console.log(meals);
+
+  let mealsHtml = '';
+  for (let i = 0; i < meals.meals.length; i++) {
+    mealsHtml += ` <div class = 'col mt-4 pe-0'>
+                    <div class="card" style="width: 20rem;">
+                    <img src="${meals.meals[i].strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body text-white ">
+                     <div class = "title-like"> 
+                     <h5 class="card-title"> ${meals.meals[i].strMeal}</h5>
+                     <span id="like${meals.meals[i].idMeal}" class="ms-4 like">🖤</span>
+                     </div>
+                      <p class="card-text"> ${meals.meals[i].strCategory}</p>
+                     <a href="#" class="btn btn-primary">Comment</a>
+                    </div>
+                </div>
+              </div>`;
+    //console.log(meals[i].strMeal);
+    result.innerHTML = mealsHtml;
+  }
+};
+
+export default displayDom;
