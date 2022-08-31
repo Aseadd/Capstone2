556d478247ee4543f147214c967f2511a381112d
Marco Oquendo <marcooquendoc@gmail.com>
2022-08-31 17:59:35 -0500

    Fix indentation

--- src/module/displayDom.js
+++ src/module/displayDom.js
@@ -4,23 +4,28 @@ const displayDom = async (url) => {
   const result = document.querySelector('.row');
   const meals = await getMeals(url);
   // const mealList = JSON.parse(meals);
-  console.log(meals);
+  // console.log(meals);
 
   let mealsHtml = '';
   for (let i = 0; i < meals.meals.length; i += 1) {
-    mealsHtml += ` <div class = 'col mt-4 pe-0'>
-                    <div class="card" style="width: 20rem;">
-                    <img src="${meals.meals[i].strMealThumb}" class="card-img-top img" alt="...">
-                    <div class="card-body text-black ">
-                     <div class = "title-like"> 
-                     <h5 class="card-title"> ${meals.meals[i].strMeal}</h5>
-                     <span id="like${meals.meals[i].idMeal}" class="ms-4 like">🖤</span>
-                     </div>
-                      <p class="card-text"> ${meals.meals[i].strCategory}</p>
-                     <a href="#" class="btn btn-primary">Comment</a>
-                    </div>
-                </div>
-              </div>`;
+    mealsHtml
+    += ` 
+    <div class='col mt-4 pe-0'>
+      <div class="card" style="width: 20rem;">
+        <img src="${meals.meals[i].strMealThumb}" class="card-img-top img" alt="...">
+        <div class="card-body text-black ">
+          <div class="title-like">
+            <h5 class="card-title"> ${meals.meals[i].strMeal}</h5>
+            <span id="like${meals.meals[i].idMeal}" class="ms-4 like">🖤</span>
+          </div>
+          <p class="card-text"> ${meals.meals[i].strCategory}</p>
+          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            Modal
+          </button>
+        </div>
+      </div>
+    </div>
+  `;
     result.innerHTML = mealsHtml;
   }
 };
