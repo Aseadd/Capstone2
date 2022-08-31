43b90587bdef6c162a5911bad28eaaf8fe740b47
Marco Oquendo <marcooquendoc@gmail.com>
2022-08-31 18:33:57 -0500

    import modalMethods Add event listener to display modal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 // import _ from 'lodash';
 import './style.css';
 import displayDom from './module/displayDom.js';
+import modalMethods from './module/modal.js';
 
 const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=a';
 displayDom(url);
+
+const allCards = document.querySelector('#allCards');
+allCards.addEventListener('click', (e) => {
+  console.log(e.target);
+  console.log(e.target.id);
+  // modalMethods.show();
+});
