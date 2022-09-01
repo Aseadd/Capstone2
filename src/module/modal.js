ec379ae26987eac20038210a67ab03cc6b2ba087
Marco Oquendo <marcooquendoc@gmail.com>
2022-09-01 18:49:55 -0500

    Update modal.js

--- src/module/modal.js
+++ src/module/modal.js
@@ -16,16 +16,16 @@ const modalMethods = {
         </div>
         <div class="modal-body">
           <img src="${src}" class="img modalImg" alt="Meal Image">
-          <form action="#" method="post" class='score-form mt-2 modalForm'>
+          <form id="form${id}" method="get" class='modalForm mt-2'>
             <h3>${category}</h3>
             <p>Your comment will be the number <span class="badge rounded-pill bg-info text-dark">32</span></p>
             <div class='justify-content-center input-group-sm'>
-              <input class="form-control formInput" type="text" placeholder="Your name" name="name" id="nameMealID">
-              <textarea class="form-control formInput mt-2" type="text" placeholder="Your comment" name="comment" id="commentMealID"></textarea>
+              <input class="form-control formInput" type="text" placeholder="Your name" name="name" id="nameMealID" required>
+              <textarea class="form-control formInput mt-2" type="text" placeholder="Your comment" name="comment" id="commentMealID" required></textarea>
             </div>
             <div class="modal-footer mt-2 ma-0">
               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <input type="button" value="Comment" class="submit score-btn btn btn-primary float-end" id="submit">
+              <input type="submit" value="Comment" class="submit btn btn-primary float-end" id="submit">
             </div>
           </form>
           <div class="commentSection">
@@ -43,13 +43,9 @@ const modalMethods = {
     const commentHtml = '';
 
     for (let i = 0; i < allComments.length; i += 1) {
-      const { idMeal } = id;
-      const { comments } = allComments.filter(
-        (e) => e.item_id === idMeal,
-      )[0] || {
-        likes: 0,
-      };
-      commentHtml.innerHTML = ` <p><span class="badge rounded-pill bg-dark">Addis Tsega 21/08/2022:</span>${comments} </p>`;
+      const idMeal1 = id.idMeal;
+      const comments1 = allComments.filter((e) => e.item_id === idMeal1)[0].comments;
+      commentHtml.innerHTML = ` <p><span class="badge rounded-pill bg-dark">Addis Tsega 21/08/2022:</span>${comments1} </p>`;
     }
     commentList.appendChild(commentHtml);
   },
