34b70e3093ba9a38c2f598c9792c83a347899f38
Marco Oquendo <marcooquendoc@gmail.com>
2022-09-01 18:49:23 -0500

    update commentApi.js

--- src/module/commentApi.js
+++ src/module/commentApi.js
@@ -2,9 +2,9 @@ const commentApi = {
   async getComments(id) {
     const idApi = 'MZ10RiH3HxYgVP46u10j';
     const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${idApi}/comments?item_id=${id}`;
-    const response = await fetch(url).then((response) => response.json());
-    const { comment } = await response.filter((e) => e.item_id === id)[0];
-    return comment;
+    const response = await fetch(url);
+    const comment = await response.json();
+    return comment.filter((ele) => ele.item_id !== id);
   },
   async addComments(id, nameValue, commentValue) {
     const idApi = 'MZ10RiH3HxYgVP46u10j';
