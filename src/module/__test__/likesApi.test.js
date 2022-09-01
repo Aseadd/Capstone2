80e900a5af0981b224ca5371d29d7379f837dc2e
addis <betsega23@gmail.com>
2022-09-02 02:12:26 +0300

    Add test files

---
+++ src/module/__test__/likesApi.test.js
@@ -0,0 +1,11 @@
+// import likesApi from '../likesApi';
+
+// it('Testing the likes API', async () => {
+//   const allLikes = await likesApi.getAllLikes();
+//   const { count } = allLikes.reduce(
+//     (accumulator, current) => accumulator + current.likes,
+//     0
+//   );
+//   console.log(count);
+//   expect(count).toEqual(50);
+// });
---
+++ src/module/meal.js
@@ -0,0 +1,14 @@
+class Meal {
+  constructor(count) {
+    this.count = count;
+  }
+
+  getCount() {
+    return this.count;
+  }
+  setCount(amount) {
+    this.count = amount;
+  }
+}
+
+export default Meal;
