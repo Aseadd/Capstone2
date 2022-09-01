8ec153d2b68f07464979d442d92fc2d888dcc3de
Marco Oquendo <marcooquendoc@gmail.com>
2022-09-01 13:03:34 -0500

    h5 to h2

--- src/module/modal.js
+++ src/module/modal.js
@@ -11,13 +11,13 @@ const modalMethods = {
     allContent.innerHTML = `
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="staticBackdropLabel">${mealName}</h5>
+          <h2 class="modal-title" id="staticBackdropLabel">${mealName}</h2>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
           <img src="${src}" class="img modalImg" alt="Meal Image">
           <form action="#" method="post" class='score-form mt-2 modalForm'>
-            <h6>${category}</h6>
+            <h3>${category}</h3>
             <p>Your comment will be the number <span class="badge rounded-pill bg-info text-dark">32</span></p>
             <div class='justify-content-center input-group-sm'>
               <input class="form-control formInput" type="text" placeholder="Your name" name="name" id="nameMealID">
