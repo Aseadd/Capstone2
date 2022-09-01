ba9e6cd57843d205a5f4f64c6fe3b82b05b4a788
Marco Oquendo <marcooquendoc@gmail.com>
2022-09-01 04:22:15 -0500

    Implement likes methods

---
+++ src/module/likesApi.js
@@ -0,0 +1,40 @@
+const likesApi = {
+  async getAllLikes() {
+    const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/MZ10RiH3HxYgVP46u10j/likes';
+    const response = await fetch(url).then((response) => response.json());
+    return response;
+  },
+  async getLikes(id) {
+    const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/MZ10RiH3HxYgVP46u10j/likes';
+    const response = await fetch(url).then((response) => response.json());
+    const { likes } = await response.filter((e) => e.item_id === id)[0];
+    return likes;
+  },
+  async addLike(id) {
+    const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/MZ10RiH3HxYgVP46u10j/likes';
+    const result = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        item_id: id,
+      }),
+    });
+    const response = await result.text();
+    return response;
+  },
+};
+
+export default likesApi;
+
+
+async recordLike(id) {
+  const result = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/q2EecIDolyiDguQmkFIj/likes/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      item_id: id,
+    }),
+  });
+  const response = await result.text();
+  return response;
+}
