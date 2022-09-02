4aaecbe659330c05e1388abe3c0373404e21771c
Marco Oquendo <marcooquendoc@gmail.com>
2022-09-02 05:52:35 -0500

    Remove uncompleted files from a different feature branch

--- src/module/__test__/meal.test.js
+++
@@ -1,9 +0,0 @@
-import Meal from '../meal.js';
-
-const meal = new Meal();
-const c = meal.getCount();
-console.log(meal.getCount());
-
-describe('If Meal length is 4', () => {
-  expect(meal.getCount()).toEqual(4);
-});
